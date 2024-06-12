31bd1ea3fda593c693eb503d0660c92797afcded
Your Name <youremail@example.com>
2024-06-12 15:44:38 +0300

    finish getting anime by id, and display it on screen

--- index.js
+++ index.js
@@ -11,15 +11,88 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "books",
+  database: "anime",
   password: "Sqlisweird112@",
   port: "5433",
 });
 db.connect();
 
+let isGenerated = false;
+let existingAnime;
+let existingPicture;
+let currentAnimeId;
+let error;
+let existingTrailer;
+let existingDescription;
+let existingTitles;
+let existingLink;
+
+let currentUserId = 1;
 //////GET REQUEST
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query(
+      "SELECT anime_list.id, anime_id, user_id FROM anime_list JOIN users ON users.id = user_id WHERE user_id = $1",
+      [currentUserId]
+    );
+    if (isGenerated) {
+      console.log(existingPicture);
+      console.log(existingTrailer);
+      console.log(existingLink);
+      console.log(existingDescription);
+      console.log(existingTitles);
+      isGenerated = false;
+      res.render("index.ejs", {
+        picture: existingPicture,
+        trailer: existingTrailer,
+        url: existingLink,
+        description: existingDescription,
+        title: existingTitles,
+      });
+    } else {
+      res.render("index.ejs");
+    }
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
+
+// post request to get value/ID then send HTTP request to get anime via API with specific ID
+app.post("/my-anime-list", async (req, res) => {
+  currentAnimeId = parseInt(req.body.animeInput);
+  let imagesArray = [];
+  let existingTitlesFiltered = [];
+  let titles = [];
+  let info = [];
+  try {
+    const response = await axios.get(
+      `https://api.jikan.moe/v4/anime/${currentAnimeId}/full`
+    );
+    const response1 = await axios.get(
+      `https://api.jikan.moe/v4/anime/${currentAnimeId}/pictures`
+    );
+
+    existingAnime = response.data.data;
+    existingTrailer = existingAnime.trailer.embed_url;
+    existingDescription = existingAnime.synopsis;
+    existingLink = existingAnime.url;
+    titles = existingAnime.titles;
+    titles.forEach((title) => {
+      if (title.type === "Default") {
+        existingTitlesFiltered.push(title.title);
+      }
+    });
+    existingTitles = existingTitlesFiltered;
+    existingPicture = existingAnime.images.jpg;
+    isGenerated = true;
+    res.redirect("/");
+  } catch (err) {
+    error = `The anime with id ${currentAnimeId} does not exist`;
+    console.log(error);
+    console.log("Error doesn't exist");
+    res.redirect("/");
+  }
 });
 
 // RUN THE SERVER
